a12b3439af5c62bc13391cb9c785372bb1db78ff
Aaron <aaron950415@qq.com>
2021-08-14 20:39:02 +1000

    Aaron:update express

--- index.ts
+++ index.ts
@@ -3,20 +3,31 @@ import { IncomingMessage, ServerResponse } from "http";
 import * as http from "http";
 import * as fs from "fs";
 import * as p from "path";
-const server=http.createServer();
-const publicDir = p.resolve(__dirname,'build/index.html')
-server.on("request",(request: IncomingMessage,response:ServerResponse)=>{
-    const {method,url,headers} = request;
-    console.log(url);
-    if(url === "/"){
-        response.setHeader('Content-Type','text/html;charset-utf-8')
-        const stream = fs.createReadStream(publicDir)
-      stream.pipe(response)
+import express from 'express';
+import path from 'path';
+const app = express();
 
-    }
-    
-})
+app.use(express.static(path.join(__dirname, 'build')));
 
-server.listen(8888)
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(9000);
+// const server=http.createServer();
+// const publicDir = p.resolve(__dirname,'build/index.html')
+// server.on("request",(request: IncomingMessage,response:ServerResponse)=>{
+//     const {method,url,headers} = request;
+//     console.log(url);
+
+//          response.setHeader('Content-Type','text/html;charset-utf-8')
+//       //   const stream = fs.createReadStream(publicDir)
+//       // stream.pipe(response)
+//     fs.readFile(__dirname+"/build/index.html",(error,data)=>{
+//       response.end(data.toString())
+//     })
+// })
+
+// server.listen(8888)
 
 
